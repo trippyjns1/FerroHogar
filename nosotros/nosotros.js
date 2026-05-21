da9361d84c01423e5ef3484f23ec51e3165072e7
trippyjns1 <jhaderinfante4@gmail.com>
2026-05-21 00:00:26 -0500

    Actualización del proyecto FerroHogar

---
+++ nosotros/nosotros.js
@@ -0,0 +1,25 @@
+const clientes = document.getElementById('clientes')
+const productos = document.getElementById('productos')
+const trabajadores = document.getElementById('trabajadores')
+
+function animarContador(elemento, numeroFinal, duracion) {
+    let numeroActual = 0
+    let incremento = numeroFinal / (duracion / 10)
+
+    let intervalo = setInterval(function() {
+        
+        numeroActual += incremento  // ← esto faltaba
+
+        if (numeroActual >= numeroFinal) {
+            numeroActual = numeroFinal
+            clearInterval(intervalo)
+        }
+
+        elemento.textContent = Math.round(numeroActual)
+
+    }, 10)
+}
+
+animarContador(clientes, 100, 2000)
+animarContador(productos, 5000, 2000)
+animarContador(trabajadores, 5, 2000)
